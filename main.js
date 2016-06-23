fec814440b4101fbb064244c3d43cacf2e14f70f
German Ibanez <gei9980@gmail.com>
2016-06-23 11:34:19 -0300

    Conexion a BD y extend de controller

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 'use strict';
+var db = {connect:'SI'};
+
 global.AppRequire = function(name)
 {
     return require(__dirname + '/App/' + name);
@@ -8,11 +10,23 @@ global.TangoRequire = function(name)
     return require(__dirname + '/Tango/' + name);
 }
 
+global.myDB = function()
+{
+    return db;
+}
+
 var App = TangoRequire('Core/App');
+var mysql = TangoRequire('Modules/DB/MySQL');
+
+//var db = mysql.getInstance();
 
 var app = new App();
 app.addRoute('/', 'PictureCtrl@query', 'GET');
 app.addRoute('/picture/:id', 'PictureCtrl@find', 'GET');
 
-
-app.run();
+mysql.connect().then(
+    function(con)
+    {
+        db = con;
+        app.run();
+    })
